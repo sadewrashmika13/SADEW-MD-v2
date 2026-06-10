0b31cf521df9f94ccaaf1e84207209ce4089f6fe
Shaviya Dev / <cybernoxx7489@gmail.com>
2026-06-10 18:17:11 +0530

    Create forward.js

---
+++ plugins/forward.js
@@ -0,0 +1,9 @@
+// plugins/forward.js - Auto Forward stub
+// handleAutoForward is called from index.js on every message
+
+async function handleAutoForward(conn, mek, sessionId) {
+    // Auto forward feature - disabled by default
+    // Customize here if needed
+}
+
+module.exports = { handleAutoForward };
